429981e5fac4a9e8a23972508e970bcc3327c3e0
chicotobi <tobias310788@googlemail.com>
2020-07-08 13:22:13 +0200

    remove unnecessary distinction of color and black

--- fun_cr_sols.m
+++ fun_cr_sols.m
@@ -10,8 +10,6 @@ M = {H,V};
 C = {HC,VC};
 dim = [dimX,dimY];
 
-with_colors = size(cmap,1)>3;
-
 for ori=1:2
     for line=1:dim(3-ori)
         str1 = strcat(pwd,'/sols/',f_str,'/ori',num2str(ori),'_line',num2str(line),'.mat');
@@ -22,78 +20,42 @@ for ori=1:2
             mkdir(strcat(pwd,"/sols/",f_str))
         end
         if ~exist(str1,'file')
-            if(with_colors)
-                blocks = M{ori}{line};
-                colors = C{ori}{line};
-                
-                ninp = numel(blocks);
-                nwhites = dim(ori) - sum(blocks);
-                next_block_is_same_color = [diff(colors)==0, 0];
-                
-                nfreewhites = nwhites - sum(next_block_is_same_color);
-                k = ninp;
-                n = nfreewhites + k;
-                a = [nfreewhites,zeros(1,ninp)];
-                t = 0;
-                h = 0;
-                nrows = alg_n_k(n,k);
-                
-                S = ones(nrows,dim(ori),"uint8");
-                for j=1:nrows
-                    idx = 1+a(1);
-                    for i=1:ninp
-                        S(j,idx:idx+blocks(i)-1) = colors(i) + 1;
-                        idx = idx + blocks(i) + next_block_is_same_color(i) + a(i+1);
-                    end
-                    if(mod(j,tell)==0)
-                        fprintf('\tshifting... %i of %i compositions\n',j,nrows);
-                    end
-                    if(j==nrows)
-                        break;
-                    end
-                    if ( 1 < t )
-                        h = 0;
-                    end
-                    h = h + 1;
-                    t = a(h);
-                    a(h) = 0;
-                    a(1) = t - 1;
-                    a(h+1) = a(h+1) + 1;
+            blocks = M{ori}{line};
+            colors = C{ori}{line};
+            
+            ninp = numel(blocks);
+            nwhites = dim(ori) - sum(blocks);
+            next_block_is_same_color = [diff(colors)==0, 0];
+            
+            nfreewhites = nwhites - sum(next_block_is_same_color);
+            k = ninp;
+            n = nfreewhites + k;
+            a = [nfreewhites,zeros(1,ninp)];
+            t = 0;
+            h = 0;
+            nrows = alg_n_k(n,k);
+            
+            S = ones(nrows,dim(ori),"uint8");
+            for j=1:nrows
+                idx = 1+a(1);
+                for i=1:ninp
+                    S(j,idx:idx+blocks(i)-1) = colors(i) + 1;
+                    idx = idx + blocks(i) + next_block_is_same_color(i) + a(i+1);
                 end
-            else
-                black = M{ori}{line};
-                ninp = numel(black);
-                nwhites = dim(ori) - sum(black);
-                nfreewhites = nwhites - (ninp-1);
-                k = ninp;
-                n = nfreewhites + k;
-                a = [nfreewhites,zeros(1,ninp)];
-                t = 0;
-                h = 0;
-                nrows = alg_n_k(n,k);
-                
-                S = ones(nrows,dim(ori),"uint8");
-                for j=1:nrows
-                    idx = 1+a(1);
-                    for i=1:ninp
-                        S(j,idx:idx+black(i)-1) = 2;
-                        idx = idx + black(i) + 1 + a(i+1);
-                    end
-                    if(mod(j,tell)==0)
-                        fprintf('\tshifting... %i of %i compositions\n',j,nrows);
-                    end
-                    if(j==nrows)
-                        break;
-                    end
-                    if ( 1 < t )
-                        h = 0;
-                    end
-                    h = h + 1;
-                    t = a(h);
-                    a(h) = 0;
-                    a(1) = t - 1;
-                    a(h+1) = a(h+1) + 1;
+                if(mod(j,tell)==0)
+                    fprintf('\tshifting... %i of %i compositions\n',j,nrows);
                 end
+                if(j==nrows)
+                    break;
+                end
+                if ( 1 < t )
+                    h = 0;
+                end
+                h = h + 1;
+                t = a(h);
+                a(h) = 0;
+                a(1) = t - 1;
+                a(h+1) = a(h+1) + 1;
             end
             fprintf('\tFinished sol O%iL%i.\n',ori,line);
             save(str1,'S','-v7.3');
