2f0c9ffc0d9b9771511d7a67f07d88f1f1bae563
chicotobi <tobias310788@googlemail.com>
2022-02-13 23:07:02 +0100

    put line check within orientation loop

--- solve_logical.m
+++ solve_logical.m
@@ -64,110 +64,99 @@ while ~finished
     if(p.verbose)
         plot_progress(colorPossible,status,cmap,iter,threshold,1,0);
     end
-    iter = iter + 1;
-    fprintf("\nIteration %i\n",iter);
-    created_line = 0;
-    colorPossible_old = colorPossible;
     for ori=1:2
-        for line=1:dim(ori)
-            if(status{ori}{line}(3)==1)
-                n_old = status{ori}{line}(2);
-                for color=1:nColors
-                    % Update
-                    tmp = posSol{ori}{line};
-                    v = (sum(tmp==color,1)>0);
-                    colorPossible(line,:,color) = v & colorPossible(line,:,color);
+        iter = iter + 1;
+        fprintf("\nIteration %i\n",iter/2);
+        colorPossible_old = colorPossible;
+        arr = cell2mat(status{ori});
+        lines = arr(arr(:,3)==1,1)';
+        for line=lines
+            n_old = status{ori}{line}(2);
+            for color=1:nColors
+                % Update
+                tmp = posSol{ori}{line};
+                v = (sum(tmp==color,1)>0);
+                colorPossible(line,:,color) = v & colorPossible(line,:,color);
 
-                    npSol = size(posSol{ori}{line},1);
-                    remove_this_solution = false(npSol,dim(3-ori));
-                    colorPossibleLine = colorPossible(line,:,color);
-                    tmp1 = (posSol{ori}{line}==color);
-                    for i = 1:dim(3-ori)
-                        remove_this_solution(:,i) = (~colorPossibleLine(i)) & tmp1(:,i);
-                    end
-                    idxKeep = ~any(remove_this_solution,2);
-                    posSol{ori}{line} = posSol{ori}{line}(idxKeep,:);
+                npSol = size(posSol{ori}{line},1);
+                remove_this_solution = false(npSol,dim(3-ori));
+                colorPossibleLine = colorPossible(line,:,color);
+                tmp1 = (posSol{ori}{line}==color);
+                for i = 1:dim(3-ori)
+                    remove_this_solution(:,i) = (~colorPossibleLine(i)) & tmp1(:,i);
+                end
+                idxKeep = ~any(remove_this_solution,2);
+                posSol{ori}{line} = posSol{ori}{line}(idxKeep,:);
 
-                    for i=1:dim(3-ori)
-                        c1 = min(posSol{ori}{line}(:,i));
-                        c2 = max(posSol{ori}{line}(:,i));
-                        if(c1==c2)
-                            for c=1:nColors
-                                if(c~=c1)
-                                    colorPossible(line,i,c) = 0;
-                                end
+                for i=1:dim(3-ori)
+                    c1 = min(posSol{ori}{line}(:,i));
+                    c2 = max(posSol{ori}{line}(:,i));
+                    if(c1==c2)
+                        for c=1:nColors
+                            if(c~=c1)
+                                colorPossible(line,i,c) = 0;
                             end
                         end
                     end
                 end
-                status{ori}{line}(2) = size(posSol{ori}{line},1);
-                n_new = status{ori}{line}(2);
-                if(p.verbose && (n_new<n_old))
-                    fprintf('O%iL%03d %s - was %s.\n',ori,line,prty(n_new),prty(n_old));
-                    plot_progress(colorPossible,status,cmap,iter,threshold,ori,line);
-                end
+            end
+            status{ori}{line}(2) = size(posSol{ori}{line},1);
+            n_new = status{ori}{line}(2);
+            if(p.verbose && (n_new<n_old))
+                fprintf('O%iL%03d %s - was %s.\n',ori,line,prty(n_new),prty(n_old));
+                plot_progress(colorPossible,status,cmap,iter,threshold,ori,line);
             end
         end
-        colorPossible = permute(colorPossible,[2,1,3]);
-    end
 
-    % If solved
-    if all(sum(colorPossible,3)==1,"all")
-        finished = true;
-        continue;
-    end
-
-    % What happens if there is no change?
-    if sum((colorPossible_old(:)-colorPossible(:)).^2)==0
-        % Update the solution numbers
-        created_line = false;
-        vals = [];
-        for ori=1:2
-            for line=1:dim(ori)
-                if(status{ori}{line}(3)==0)
-                    blocks = M{ori}{line};
-                    colors = C{ori}{line};
-                    l = dim(3-ori);
-                    if(ori==1)
-                        colPos = squeeze(colorPossible(line,:,:))';
-                    else
-                        colPos = squeeze(colorPossible(:,line,:))';
-                    end
-                    [~,count] = cr_sol_rec_with_info(blocks,colors,l,colPos,1,p);
-                    if(count < threshold)
-                        created_line = true;
-                        posSol{ori}{line} = cr_sol_rec_with_info(blocks,colors,l,colPos,0,p)+1;
-                        status{ori}{line} = [line count 1];
-                        if(p.verbose)
-                            fprintf("O%iL%03d %s - created by threshold.\n",ori,line,prty(count));
-                        end
-                    else
-                        vals = [vals;ori line count];
-                    end
-                end
-            end
+        % If solved
+        if all(sum(colorPossible,3)==1,"all")
+            finished = true;
+            break;
         end
-        % If no new line is created, choose some with smallest count
-        if ~created_line
-            vals = sortrows(vals,3);
-            nline = min(3,size(vals,1));
-            for i=1:nline
-                ori = vals(i,1);
-                line = vals(i,2);
+
+        % What happens if there is no change?
+        if sum((colorPossible_old(:)-colorPossible(:)).^2)==0
+            % Update the solution numbers
+            created_line = false;
+            arr = cell2mat(status{ori});
+            lines = arr(arr(:,3)==0,1)';
+            for line=lines
                 blocks = M{ori}{line};
                 colors = C{ori}{line};
                 l = dim(3-ori);
-                if(ori==1)
-                    colPos = squeeze(colorPossible(line,:,:))';
+                colPos = squeeze(colorPossible(line,:,:))';
+                [~,count] = cr_sol_rec_with_info(blocks,colors,l,colPos,1,p);
+                if(count < threshold)
+                    created_line = true;
+                    posSol{ori}{line} = cr_sol_rec_with_info(blocks,colors,l,colPos,0,p)+1;
+                    status{ori}{line} = [line count 1];
+                    if(p.verbose)
+                        fprintf("O%iL%03d %s - created by threshold.\n",ori,line,prty(count));
+                    end
                 else
-                    colPos = squeeze(colorPossible(:,line,:))';
+                    status{ori}{line} = [line count 0];
                 end
-                posSol{ori}{line} = cr_sol_rec_with_info(blocks,colors,l,colPos,0,p) + 1;
-                if(p.verbose)
-                    fprintf("O%iL%03d %s - created by force.\n",ori,line,prty(vals(i,3)));
+            end
+            % If no new line is created, choose some with smallest count
+            if ~created_line
+                arr = sortrows(cell2mat(status{ori}),2);
+                lines = arr(arr(:,3)==0,1)';
+                for i=1:min(3,numel(lines))
+                    line = lines(i);
+                    blocks = M{ori}{line};
+                    colors = C{ori}{line};
+                    l = dim(3-ori);
+                    colPos = squeeze(colorPossible(line,:,:))';
+                    posSol{ori}{line} = cr_sol_rec_with_info(blocks,colors,l,colPos,0,p) + 1;
+                    status{ori}{line}(3) = 1;
+                    if(p.verbose)
+                        fprintf("O%iL%03d %s - created by force.\n",ori,line,prty(status{ori}{line}(2)));
+                    end
                 end
             end
         end
+
+        colorPossible = permute(colorPossible,[2,1,3]);
     end
     t = toc;
     f1 = fopen("results.csv","a");
